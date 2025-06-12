5ff30209044bc3e33482f10a84f3697782bac1b4
Prakhar Verma <prakharverma2019@gmail.com>
2025-06-12 21:04:53 +0530

    removed create table code

--- database.js
+++ database.js
@@ -13,22 +13,22 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-(async () => {
-  try {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS schools (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        address VARCHAR(255),
-        latitude FLOAT,
-        longitude FLOAT
-      )
-    `;
-    const [result] = await pool.execute(createTableQuery);
-    console.log("Table checked/created.");
-  } catch (err) {
-    console.error("Error creating table:", err.message);
-  }
-})();
+// (async () => {
+//   try {
+//     const createTableQuery = `
+//       CREATE TABLE IF NOT EXISTS schools (
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name VARCHAR(100) NOT NULL,
+//         address VARCHAR(255),
+//         latitude FLOAT,
+//         longitude FLOAT
+//       )
+//     `;
+//     const [result] = await pool.execute(createTableQuery);
+//     console.log("Table checked/created.");
+//   } catch (err) {
+//     console.error("Error creating table:", err.message);
+//   }
+// })();
 
 export default pool;
